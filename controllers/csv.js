de315b55c36136ebc0e931ce5c42a96c758e48a1
deepali <deepalisonavane5751@gmail.com>
2022-01-19 21:47:01 +0530

    use multer to upload file

--- controllers/csv.js
+++ controllers/csv.js
@@ -1,6 +1,12 @@
 const Csv = require('../models/csv');
 
 exports.csvUpload = (req, res) => {
+    var csvString = req.file
+    //  converter.fromString(csvString, function(err,result){
+    //      if(err)return res.send("ERR")
+    //      res.send(result);
+    //  }); 
+    console.log("file ::", res.json(csvString));
     res.json({
         message: 'Initial step Sucessfully'
     });
--- routes/csv.js
+++ routes/csv.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const multer  = require('multer')
 
 const { csvUpload } = require('../controllers/csv.js');
 
-router.get('/csv-upload', csvUpload);
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname);
+    }
+  })
+
+const upload = multer({ storage: storage }).single('csvfile');
+
+router.post('/csv-upload', upload, csvUpload);
 
 module.exports = router;
